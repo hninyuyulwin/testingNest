09353f97db2cb93194a97e0e77a5ff71b6f5b0c1
hninyuyulwin <hninyuyulwin01@gmail.com>
2024-10-15 13:06:33 +0630

    first-commit

---
+++ src/property/property.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('property')
+export class PropertyController {
+    @Get()
+    findAll(){
+        return "This is all properties"
+    }
+
+    @Get(":id")
+    getByID(@Param() id){
+        return id;
+    }
+
+    @Post()
+    createProperty(){
+        return "Posting property";
+    }
+}
+
